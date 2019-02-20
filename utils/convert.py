4c6765424cb0f71348c2ff3e7b2bb92933c915b9
gullabi <bkulebi@gmail.com>
2019-02-20 16:40:34 +0000

    download option added to convert

--- utils/convert.py
+++ utils/convert.py
@@ -1,8 +1,11 @@
 import os
 import json
+import logging
 from copy import deepcopy
+from subprocess import call, check_output, DEVNULL
+from retrying import retry
 
-FILE = 'test/parlament_processed_sessions_sh.json'
+FILE = 'test/parlament_processed_sessions.json'
 BASE = '../parlament-scrape/audio'
 
 def main():
@@ -16,7 +19,7 @@ def main():
             else:
                 uri = get_uri(value['urls'][0][1], basepath)
                 if not uri:
-                    print('%s not found'%value['urls'][0][1])
+                    print('%s still not found'%value['urls'][0][1])
                 value['urls'][0][1] = uri
                 value['source'] = yaml
                 new_value = deepcopy(value)
@@ -35,10 +38,51 @@ def get_uri(url, basepath):
                                        basename[0],
                                        basename[1],
                                        basename))
+    if not os.path.isfile(uri):
+        logging.info('attempting to download %s'%url)
+        curl_download(url, uri)
     if os.path.isfile(uri):
         return uri
     else:
         return None
+
+@retry(stop_max_attempt_number=3, wait_fixed=1000)
+def curl_download(uri, filepath):
+    msg = 'checking %s'%uri
+    logging.info(msg)
+    # check the http headers
+    status, uri = get_status_code(uri)
+    if status == 302:
+        # redirect uri should have been extracted to the uri variable
+        status, uri = get_status_code(uri)
+    if status != 200:
+        error = 'the resource in the url %s cannot be reached'\
+                              ' with status %i.'%(uri,status)
+        logging.error(error)
+        if status == 401:
+            return None
+        else:
+            raise ConnectionError(error)
+
+    # create file
+    with open(filepath,'w') as fout:
+        cmd = ['curl','-g',uri]
+        logging.info("downloading %s"%uri)
+        call(cmd, stdout=fout, stderr=DEVNULL) #seems dangerous but 404s are
+                                               #caught by the get_status_code
+def get_status_code(url):
+    cmd = ['curl','-I',url]
+    header = check_output(cmd, stderr=DEVNULL)
+    header_list = header.split(b'\n')
+    code = int(header_list[0].split()[1])
+    uri = url
+    if code == 302:
+        for h in header_list:
+            if h.startswith(b'Location: '):
+                uri = h.strip().decode('ascii')[10:]
+                if 'http' not in uri:
+                    code = 401
+    return code, uri
 
 def get_new_key(ple_code, uri):
     no = os.path.basename(uri).split('.')[0]
