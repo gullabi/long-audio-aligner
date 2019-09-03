e47f75ccb74c15e8ba935c4a9c1797e9c1710cc4
gullabi <bkulebi@gmail.com>
2019-09-03 18:54:59 +0200

    minor cleaning in aligner test case

--- tests/test_aligner.py
+++ tests/test_aligner.py
@@ -16,8 +16,9 @@ class AlignerTestCase(unittest.TestCase):
         #test_aligner = Align()
         align_test_wav = os.path.join(TEST_FILES_PATH,
                                       'c3d9d2a15a76a9fbb591.mp3')
-        align_test_text = open(os.path.join(TEST_FILES_PATH,
-                                '2013_06_05_57807-14-c3d9d2a15a76a9fbb591.txt')).read()
+        with open(os.path.join(TEST_FILES_PATH,
+                    '2013_06_05_57807-14-c3d9d2a15a76a9fbb591.txt')) as infile:
+            align_test_text = infile.read()
         self.aligner = Align(align_test_wav, align_test_text, DICT_PATH, TMP_PATH)
         self.decode_outfile = self.aligner.align_outfile.replace('_align','_decode')
         self.cs = CMU(MODEL_PATH)
@@ -32,12 +33,14 @@ class AlignerTestCase(unittest.TestCase):
         # create corpus
         self.aligner.create_textcorpus()
         # assert that corpus has more than one line
-        self.assertTrue(len(open(self.aligner.corpus).readlines()) > 1)
+        with open(self.aligner.corpus) as infile:
+            self.assertTrue(len(infile.readlines()) > 1)
 
         # create lm file
         self.aligner.create_lm()
         # check its format
-        last_line = open(self.aligner.lm).readlines()[-1].strip()
+        with open(self.aligner.lm) as infile:
+            last_line = infile.readlines()[-1].strip()
         self.assertEqual(last_line, '\\end\\', msg='LM file has wrong format')
 
         # convert audio
@@ -46,6 +49,7 @@ class AlignerTestCase(unittest.TestCase):
         # decode
         self.cs.decode(self.aligner.audio_raw, self.aligner.lm)
         self.assertTrue(len(self.cs.segs) > 1, msg="decode did not yield results")
+
         '''
         with open(self.decode_outfile, 'w') as out:
             json.dump(self.cs.segs, out, indent=2)
