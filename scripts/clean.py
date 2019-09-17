36a44203813111dec819d0e967c2a081132ca28c
gullabi <bkulebi@gmail.com>
2019-09-17 20:06:59 +0200

    cleaning script modified for the actual db

--- scripts/clean.py
+++ scripts/clean.py
@@ -9,10 +9,19 @@ from pymongo import MongoClient
 from multiprocessing.dummy import Pool
 from tqdm import tqdm
 
-DOT = re.compile('(?<=[^l])·(?=[^l])')
+RE_DOT = '((?<=[^l])·(?=[^l])|(?<=[^l])·|·(?=[^l]))'
+DOT = re.compile(RE_DOT)
+S = '(?=[,.?!: ])'
 
 def main():
-    db = db_connect()
+    clean('intervention')
+
+def clean(option):
+    if option == 'intervention':
+        db = db_connect('v3')
+    else:
+        db = db_connect('aggregate_mas_v2')
+
     outdir = './'
     process_list = []
     for element in db.find():
@@ -21,49 +30,76 @@ def main():
     logging.info('sending the list to be processed')
     c_tuples = get_clean_list()
 
-    for c_t in c_tuples:
-        print(c_t)
-        for element in db.find({'Innerfield.words':{'$regex': c_t[0]}}):
+    if option == 'intervention':
+        for ct in c_tuples:
+            print(ct)
+            for element in db.find():
+                for i, speaker_text in enumerate(element['value']['text']):
+                    speaker, text = speaker_text
+                    find = re.search(ct[0], text)
+                    if find:
+                        #print(text)
+                        element['value']['text'][i][1] = re.sub(ct[0], ct[1], text)
+                        db.save(element)
+        for element in db.find():
+            for i, speaker_text in enumerate(element['value']['text']):
+                speaker, text = speaker_text
+                find = re.search(RE_DOT, text)
+                if find:
+                    if find.start()-10 < 0:
+                        start = 0
+                    else:
+                        start = find.start()-10
+                    print(text[start:start+20])
+                    element['value']['text'][i][1] = DOT.sub('', text)
+                    db.save(element)
+    else:
+        for c_t in c_tuples:
+            print(c_t)
+            for element in db.find({'Innerfield.words':{'$regex': c_t[0]}}):
+                element['Innerfield']['words'] = \
+                              element['Innerfield']['words'].replace(c_t[0],c_t[1])
+                db.save(element)
+        for element in db.find({'Innerfield.words':{'$regex': RE_DOT}}):
             element['Innerfield']['words'] = \
-                          element['Innerfield']['words'].replace(c_t[0],c_t[1])
+                                        DOT.sub('', element['Innerfield']['words'])
             db.save(element)
-    for element in db.find({'Innerfield.words':{'$regex': '[^l]·[^l]'}}):
-        element['Innerfield']['words'] = \
-                                    DOT.sub('', element['Innerfield']['words'])
-        db.save(element)
 
 def get_clean_list():
-    return [(' miler ', ' mil '),
-            ('-un ','-u '),
-            ('·li ','-li '),
-            ('·hi ','-hi '),
-            ('·la ','-la '),
+    return [(' miler'+S, ' mi '),
+            ('-un'+S,'-u'),
+            ('·li'+S,'-li'),
+            ('·hi'+S,'-hi'),
+            ('·la'+S,'-la'),
             ('·i·','-i-'),
             ('to· te','tote'),
-            ('·ne ','-ne '),
-            ('·me ','-me '),
-            ('·te ','-te '),
-            ('·ho ','-ho '),
-            ('·lo ','-lo '),
-            ('·nos ','-nos '),
-            ('·vos ','-vos '),
-            ('·los ','-los '),
-            ('·se ','-se '),
+            ('·ne'+S,'-ne'),
+            ('·me'+S,'-me'),
+            ('·te'+S,'-te'),
+            ('·ho'+S,'-ho'),
+            ('·lo'+S,'-lo'),
+            ('·nos'+S,'-nos'),
+            ('·vos'+S,'-vos'),
+            ('·los'+S,'-los'),
+            ('·les'+S,'-les'),
+            ('·se'+S,'-se'),
+            ('·u'+S,'-u'),
             ('no·soluci','no-soluci'),
             ('sánchez·camacho','sánchez-camacho'),
-            ('·dos ','-dos '),
-            ('·tres ','-tres '),
-            ('·quatre ','-quatre '),
-            ('·cinc ','-cinc '),
-            ('·sis ','-sis '),
-            ('·set ','-set '),
-            ('·vuit ','-vuit '),
-            ('·nou ','-nou ')]
+            ('·dos'+S,'-dos'),
+            ('·tres'+S,'-tres'),
+            ('·quatre'+S,'-quatre'),
+            ('·cinc'+S,'-cinc'),
+            ('·sis'+S,'-sis'),
+            ('·set'+S,'-set'),
+            ('·vuit'+S,'-vuit'),
+            ('·nou'+S,'-nou'),
+            ('a·bandera','a bandera'),
+            ('·(?=[A-ZÀÁÉÈÜÚÍÏÓÒÇ])', ' ')]
 
-def db_connect():
+def db_connect(colname):
     client = MongoClient('localhost',27017)
     dbname = 'parlament'
-    colname = 'aggregate_mas_v2'
     db = client[dbname]
     return db[colname]
 
